05a47bc7561d81ddc3d48b67e37906ff9289bc28
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-02-27 16:42:14 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -14,9 +14,9 @@ async def awake(event):
   tai = event.sender.first_name
   YUMEKO = "**Hello I'm Chrollo!** \n\n"
   YUMEKO += "»**Database is working properly** \n\n"
-  YUMEKO += "×**Owner: [Hunter Committee]](https://t.me/Hunter_Committee), [Freak](https://t.me/Freaking_tag)** \n\n"
-  YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
-  YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
+  YUMEKO += "»**Owner: [Hunter Committee]](https://t.me/Hunter_Committee), [Freak](https://t.me/Freaking_tag)** \n\n"
+  YUMEKO += f"»**Telethon Version : {tlhver}** \n\n"
+  YUMEKO += f"»**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Chrollo_Bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Phantom_Troupes")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
