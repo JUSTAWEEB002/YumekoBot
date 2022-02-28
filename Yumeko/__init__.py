e7adf1bf2a12791508dfc8b83c78c0391d774a4d
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-02-28 20:11:15 +0530

    Update __init__.py

--- Yumeko/__init__.py
+++ Yumeko/__init__.py
@@ -184,7 +184,7 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 ZODIAC.add(OWNER_ID)
-TROUPE_MEMBER.add(TROUPE_MEMBER)
+TROUPE_MEMBER.add(OWNER_ID)
 DEV_USERS.add(1007275387)
 DEV_USERS.add(1007275387)
 DEV_USERS.add(1007275387)
