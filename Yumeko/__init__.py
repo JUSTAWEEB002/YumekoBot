8ac42191c0452e5b313ed4ef2af0d0110fe0e29f
JUSTAWEEB002 <100307135+JUSTAWEEB002@users.noreply.github.com>
2022-02-28 21:12:54 +0530

    Update __init__.py

--- Yumeko/__init__.py
+++ Yumeko/__init__.py
@@ -43,23 +43,23 @@ if ENV:
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        ZODIAC = set(int(x) for x in os.environ.get("ZODIAC", "").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
         DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        ZODYCK = set(int(x) for x in os.environ.get("DEMONS", "").split())
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        BOUNTY_HUNTER = set(int(x) for x in os.environ.get("HUNTER", "").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-        HUNTER = set(int(x) for x in os.environ.get("BOUNTY HUNTER", "").split())
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
@@ -183,11 +183,11 @@ else:
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
-OWNER_ID.add(OWNER_ID)
+DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1007275387)
-DEV_USERS.add(1007275387)
-DEV_USERS.add(1007275387)
+DEV_USERS.add(1963422158)
+DEV_USERS.add(1817146787)
+DEV_USERS.add(1138045685)
 
 if not SPAMWATCH_API:
     sw = None
